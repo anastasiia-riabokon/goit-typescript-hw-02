feb4ed9a4ed478b7716d65f8bde131131039efd7
Anastasiia Riabokon <a.ryabokon20@gmail.com>
2024-07-10 10:09:34 +0300

    feat(app): ImageCard typing

---
+++ src/components/ImageCard/ImageCard.tsx
@@ -0,0 +1,23 @@
+const ImageCard = ({alt_description, urls, likes, tags, description, links, user}: Photo, {onClick}:OnClickImage) => {
+  return (
+    <>
+      <img
+        src={urls.small}
+        alt={alt_description}
+        onClick={() =>
+          onClick({
+            alt: alt_description,
+            src: urls.regular,
+            likes,
+            tags,
+            description,
+            download: links.download,
+            user,
+          })
+        }
+        className="shadow-lg shadow-neutral-400 hover:shadow-slate-500 transition-shadow duration-300 ease-in-out rounded-md cursor-pointer object-cover w-full h-full"
+      />
+    </>
+  );
+};
+export default ImageCard;
