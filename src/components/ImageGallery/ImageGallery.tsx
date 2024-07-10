9280c9ea74a2b603525ed93a508d9900507af3dc
Anastasiia Riabokon <a.ryabokon20@gmail.com>
2024-07-10 13:09:59 +0300

    refactor(app): delete setTimeout and +w&h for li (for smooth scrolling)

--- src/components/ImageGallery/ImageGallery.tsx
+++ src/components/ImageGallery/ImageGallery.tsx
@@ -11,22 +11,18 @@ const ImageGallery = ({photos,onClick}:ImageGalleryProps) => {
       isFirstRender.current = false;
       return;
     }
-    const scrollToNewPhotos = () => {
-      if (photoRef.current) {
+    if (photoRef.current) {
         const heightEl = photoRef.current.getBoundingClientRect().height;
         window.scrollBy({
           top: heightEl * 1.5,
           behavior: "smooth",
         });
       }
-    };
-
-    setTimeout(scrollToNewPhotos, 100);
   }, [photos]);
   return (
     <ul className="flex gap-6 flex-wrap justify-center">
       {photos.map((item:Photo) => (
-        <li key={item.id} className="gallery-item" ref={photoRef}>
+        <li key={item.id} className="w-[400px] h-[275px]" ref={photoRef}>
           <ImageCard {...item} onClick={onClick} />
         </li>
       ))}
